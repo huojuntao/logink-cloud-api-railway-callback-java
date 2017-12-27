6931756f5a48c925775764f13af3cdf5c45e2ed8
huojuntao <33516616+huojuntao@users.noreply.github.com>
2017-12-27 17:23:55 +0800

    add 2 fields

--- src/main/java/org/logink/cloud/railway/callback/dto/CallbackSendData.java
+++ src/main/java/org/logink/cloud/railway/callback/dto/CallbackSendData.java
@@ -1,10 +1,21 @@
 package org.logink.cloud.railway.callback.dto;
 
+/**
+ * 
+ * @ClassName: CallbackSendData
+ * @Description: Updated Resource information class.
+ * @author: yinjd
+ * @date: 2017年8月7日 上午8:58:13
+ *
+ */
+
 public class CallbackSendData {
 
 	private String taskId;
-	private String freightTransportStationName; //当前到站
+	private String freightTransportBureauName;  //当前到达局
+	private String freightTransportStationName; //当前到达站
 	private String dateTime;
+	private String arrivalMileage;
 	
 	public String getTaskId() {
 		return taskId;
@@ -24,10 +35,24 @@ public class CallbackSendData {
 	public void setDateTime(String dateTime) {
 		this.dateTime = dateTime;
 	}
+	
+	public String getFreightTransportBureauName() {
+		return freightTransportBureauName;
+	}
+	public void setFreightTransportBureauName(String freightTransportBureauName) {
+		this.freightTransportBureauName = freightTransportBureauName;
+	}
+	public String getArrivalMileage() {
+		return arrivalMileage;
+	}
+	public void setArrivalMileage(String arrivalMileage) {
+		this.arrivalMileage = arrivalMileage;
+	}
 	@Override
 	public String toString() {
-		return "CallbackSendData [taskId=" + taskId + ", freightTransportStationName=" + freightTransportStationName
-				+ ", dateTime=" + dateTime + "]";
+		return "CallbackSendData [taskId=" + taskId + ", freightTransportBureauName=" + freightTransportBureauName
+				+ ", freightTransportStationName=" + freightTransportStationName + ", dateTime=" + dateTime
+				+ ", arrivalMileage=" + arrivalMileage + "]";
 	}
-	
+
 }
